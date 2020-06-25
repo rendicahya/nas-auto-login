1ba81121f1fcb56e56ad732d346040d280c4456d
Randy Cahya Wihandika <rendicahya@gmail.com>
2020-06-25 16:19:15 +0700

    Editing message.

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import json
 import logging
+from datetime import datetime
 from os import getenv
 from time import sleep
 
 from dotenv import load_dotenv
 from requests import post
 
+time_format = '%a %d-%b-%Y %H:%M:%S'
+
 load_dotenv()
 logging.basicConfig(filename='access.log', level=logging.INFO, format='%(asctime)s: %(message)s',
-                    datefmt='%a %d-%b-%Y %H:%M:%S')
+                    datefmt=time_format)
 
 url = 'http://nas.ub.ac.id/ac_portal/login.php'
 interval = getenv('NAS_INTERVAL')
@@ -21,7 +24,8 @@ data = {'opr': 'pwdLogin',
 while True:
     res = post(url, data=data).content.decode('utf-8').replace("'", '"')
     res_json = json.loads(res)
+    now = datetime.now().strftime(time_format)
 
-    print('Success:' if res_json['success'] else 'Failure:', res_json['msg'])
+    print(now, 'Success:' if res_json['success'] else 'Failure:', res_json['msg'])
     logging.info(res)
     sleep(int(interval) * 60)
